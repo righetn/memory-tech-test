61be6c75939afc41e9bacf37c77aec40393d2dad
Nicolas Righetti <nicolasrighetti@MacBook-Pro-de-Nicolas.local>
2020-09-06 20:55:29 +0200

    change root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'dashboard/index'
-  get 'dashboard/infos'
+  root 'dashboard#index'
+  get 'dashboard/index', 'dashboard#index'
+  get 'dashboard/infos', 'dashboard#infos'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
